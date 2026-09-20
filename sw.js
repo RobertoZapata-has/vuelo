be7079a6a6a59b58c5b31dfd45e1a28e0e2204c8
RobertoZapata-has <magnuscleto@gmail.com>
2026-09-20 20:07:40 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vuelo-v1';
+const CACHE = 'vuelo-v2';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
